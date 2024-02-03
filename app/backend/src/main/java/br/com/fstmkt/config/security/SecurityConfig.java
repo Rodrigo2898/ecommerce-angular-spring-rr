6b2482040ca5bff403b6e12a1c50d415bb0a9c0b
Rodrigo2898 <rodrigofeitosarodrigues@gmail.com>
2024-02-02 22:47:30 -0300

    Security configurado

--- app/backend/src/main/java/br/com/fstmkt/config/security/SecurityConfig.java
+++ app/backend/src/main/java/br/com/fstmkt/config/security/SecurityConfig.java
@@ -2,11 +2,24 @@ package br.com.fstmkt.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +30,42 @@ public class SecurityConfig {
     public SecurityConfig(JwtEntryPoint jwtEntryPoint, SecurityFilter securityFilter) {
         this.jwtEntryPoint = jwtEntryPoint;
         this.securityFilter = securityFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((exception) -> exception
+                        .authenticationEntryPoint(jwtEntryPoint))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(antMatcher(HttpMethod.POST, "/api/v1/auth/**")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.PUT, "api/v1/usuario/senha")).permitAll()
+                        .requestMatchers("/api/v1/usuario/listar").hasAnyAuthority("ADMIN", "CLIENTE", "VENDEDOR")
+                        .requestMatchers("/api/v1/usuario/perfil").hasAnyAuthority("ADMIN", "CLIENTE", "VENDEDOR")
+                        .requestMatchers("/api/v1/usuario/**").hasAuthority("ADMIN")
+                        .requestMatchers("/api/v1/vendas/**").hasAnyAuthority("ADMIN", "VENDEDOR")
+                        .requestMatchers("/api/v1/produtos/**").hasAnyAuthority("ADMIN", "VENDEDOR")
+                        .requestMatchers("/api/v1/carrinho/**").hasAnyAuthority("VENDEDOR", "CLIENTE")
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+
+        return source;
     }
 
     @Bean
